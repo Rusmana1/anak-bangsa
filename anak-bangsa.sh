cd99c71d14aa0b9972d0918cb94f01822323c1af
Rusmana <39558261+Rusmana1@users.noreply.github.com>
2018-05-29 21:45:26 +0700

    Add files via upload

---
+++ anak-bangsa.sh
@@ -0,0 +1,59 @@
+#! / system / xbin / bash
+# gunakanlah dengan bijak
+# lebih baik berkarya dari pada bergaya
+clear
+biru  = ' \ 033 [34; 1m '
+
+hijau = ' \ 033 [32; 1m ' 
+
+ungu  = ' \ 033 [35; 1m '
+
+cyan  = ' \ 033 [36; 1m '
+
+merah = ' \ 033 [31; 1m '
+
+putih = ' \ 033 [37; 1m ' 
+
+figlet Mr_tr3v!0n
+echo " "
+echo  " \ 033 [34; 1mdibuat oleh: Mr_tr3v!0n  "
+echo  " \ 033 [31; 1mhubungi saya: 083879017166  "
+echo  " \ 033 [35; 1mSalam Anak Bangsa  "
+tidur 1
+echo  " \ 033 [32; 1m [+] ========================================== [+]  "
+echo  " \ 033 [34; 1mPilih yg mau di eksekusi  "
+echo  " \ 033 [36;  "
+echo  " \ 033 [31; 1m [*] --------------------------------------- [*]  "
+echo  $ blue   " 1 DDOS attack= >>>  "
+echo  " \ 033 [31; 1m [*] --------------------------------------- [*]  "
+echo  " \ 033 [32; 1m [+] ========================================== [+]  "
+read  -p   "PILIH YG INGIN DI EKSEKUSI"
+
+if [  $ind = 1 ] | | [ $ ind = 1 ]
+then 
+clear 
+figlet Mr_tr3v!0n
+echo  $red  "mudah-mudah bisa di pakai dan pakailah dengan bijak"
+sleep 1
+cd Test
+pip2 install requests
+git clone https://github.com/Rusmana1/anak-bangsa
+cd anak-bangsa
+python2 DDOS.py
+fi
+
+if [ $ind = 0 ] || [ $ind = 0 ]
+
+then
+
+echo "\033[36;1mThank you for waching1!1!1!"
+
+sleep 5
+
+echo "\033[35;1mmudah-mudahan  bisa di pakai dan pakailah dengan bijak"
+
+sleep 3
+
+exit
+
+fi
